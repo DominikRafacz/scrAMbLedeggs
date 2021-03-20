4252b3d4b5677863fe23ecc90740b1d5c8b01973
Laura <erdaradun.gaztea@gmail.com>
2021-03-20 14:44:09 +0100

    sorry, had to implement sth; implemented most measures

---
+++ functions/measures.R
@@ -0,0 +1,17 @@
+accuracy <- function(label, prediction) {
+  mean(label == prediction)
+}
+
+precision <- function(label, prediction) {
+  sum(label == 1 & prediction == 1)/sum(prediction == 1)
+}
+
+recall <- function(label, prediction) {
+  sum(label == 1 & prediction == 1)/sum(label == 1)
+}
+
+F_measure <- function(label, prediction, beta = 1) {
+  prec_score <- precision(label, prediction)
+  recall_score <- recall(label, prediction)
+  (1 + beta^2) * prec_score * recall_score / (beta^2 * prec_score + recall_score)
+}
