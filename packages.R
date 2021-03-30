ea4b783547d256ad17a3286581c13f2a43b87a30
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-03-31 00:50:10 +0200

    removed initial structure

--- packages.R
+++
@@ -1,9 +0,0 @@
-required_packages <- c("targets",
-                       "dplyr")
-
-for (package in required_packages) {
-  if (!require(package, quietly = TRUE, character.only = TRUE)) {
-    install.packages(package)
-    library(package, character.only = TRUE)
-  }
-}
