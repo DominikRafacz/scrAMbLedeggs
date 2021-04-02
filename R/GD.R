678832a37bd86e6d87511960c24278554131c3ce
Laura <erdaradun.gaztea@gmail.com>
2021-04-02 10:19:41 +0200

    moved learning rate to argument of GD

--- R/GD.R
+++ R/GD.R
@@ -1,4 +1,4 @@
-GD <- function(X, y) {
+GD <- function(X, y, learning_rate = .01) {
   X <- cbind(1, as.matrix(X))
   n <- length(y)
   y <- matrix(y, nrow = n)
@@ -7,8 +7,7 @@ GD <- function(X, y) {
   
   for (i in 1:1000) {
     first_derivative <- t(X) %*% (y - p)
-    # optimal learning rate can be computed for each iteration separately
-    learning_rate <- .01
+    # optimal learning rate cuold be computed for each iteration separately
     beta <- beta + learning_rate * first_derivative
     p <- 1 / (1 + exp(-X %*% beta))
   }
