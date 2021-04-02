1c3c8fe47a9c3f351fd30ebc9f75c83bb55582e1
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-04-02 22:09:33 +0200

    fixed predict

--- R/predict.R
+++ R/predict.R
@@ -1,6 +1,6 @@
 predict.scrAMbodeL <- function(object, newdata, ...) {
   X <- cbind(1, as.matrix(newdata))
-  probability <- 1 / (1 + exp(-X %*% beta))
+  probability <- 1 / (1 + exp(-X %*% object$beta))
   prediction <- round(probability)
   data.frame(prediction, probability)
 }
