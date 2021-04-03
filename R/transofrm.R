0a32ed1bc98d8441849d7898a51f78d8b2ad62c7
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-04-03 16:51:46 +0200

    upgraded dummify

--- R/transofrm.R
+++ R/transofrm.R
@@ -1,1 +1,5 @@
-dummify <- function(x) map(levels(x), function(y) as.integer(x == y)) %>% as_tibble
+dummify <- function(column) {
+  name <- as_label(enquo(column))
+  values <- unique(column)
+  map_df(set_names(values, paste0(name, "_", values)), function(value) as.integer(column == value))
+}
