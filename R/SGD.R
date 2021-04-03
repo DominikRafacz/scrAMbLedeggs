6f37ffc81c73fc6280c96b363e41e39d30617ffa
Laura <erdaradun.gaztea@gmail.com>
2021-04-03 14:11:33 +0200

    implemented SGD (hopefully avoided most performance traps)

---
+++ R/SGD.R
@@ -0,0 +1,24 @@
+SGD <- function(X, y, max_iter = 10, learning_rate = .01) {
+  X <- cbind(1, as.matrix(X))
+  n <- length(y)
+  beta <- matrix(0, nrow = ncol(X))
+  
+  for (i in seq_len(max_iter)) {
+    # reshuffle data before each iteration
+    new_obs_order <- sample.int(n)
+    X <- X[new_obs_order, ]
+    y <- y[new_obs_order]
+    for (j in seq_along(y)) {
+      # first we compute only that one prediction we use
+      # as.numeric converts 1x1 matrix to scalar
+      prediction <- as.numeric(1 / (1 + exp(-X[j, ] %*% beta)))
+      first_derivative <- X[j, ] * (y[j] - prediction)
+      beta <- beta + learning_rate * first_derivative
+    }
+  }
+  
+  structure(list(beta = beta),
+            parameters = list(max_iter = max_iter,
+                              learning_rate = learning_rate),
+            class = c("SGD", "scrAMbodeL"))
+}
--- _targets.R
+++ _targets.R
@@ -2,6 +2,7 @@ library(targets)
 library(tarchetypes)
 
 source("R/GD.R")
+source("R/SGD.R")
 source("R/IRLS.R")
 source("R/measures.R")
 source("R/predict.R")
@@ -23,6 +24,6 @@ list(
   tar_target(dataset_1_raw, read_csv(dataset_1_raw_file)),
   tar_target(X_1, dataset_1_raw[, -1]),
   tar_target(y_1, dataset_1_raw[, 1] == "M"),
-  tar_map(list(algorithm = rlang::syms(c("GD", "IRLS"))),
+  tar_map(list(algorithm = rlang::syms(c("GD", "IRLS", "SGD"))),
           tar_target(CV, perform_CV(algorithm, X_1, y_1)))
 )
