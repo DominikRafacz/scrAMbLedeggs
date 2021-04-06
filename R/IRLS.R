83731bd4c31bed9b9d4847fc30295f004bbb0c9c
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-04-06 16:53:27 +0200

    fixed dealing with singularity in case of IRLS

--- R/IRLS.R
+++ R/IRLS.R
@@ -1,4 +1,4 @@
-IRLS <- function(X, y, max_iter = 10) {
+IRLS <- function(X, y, max_iter = 100, min_iter = 10) {
   X <- cbind(1, as.matrix(X))
   n <- length(y)
   y <- matrix(y, nrow = n)
@@ -7,8 +7,16 @@ IRLS <- function(X, y, max_iter = 10) {
   beta <- matrix(0, nrow = ncol(X))
 
   for (i in seq_len(max_iter)) {
-    tryCatch({inverse <- solve(t(X) %*% diag(as.vector(p * (1 - p))) %*% X)},
-             error = {break})
+    inverse <-  tryCatch(solve(t(X) %*% diag(as.vector(p * (1 - p))) %*% X),
+                         error = function(e) "singular")
+    if (inverse == "singular") {
+      if (i <= min_iter) {
+        inverse <-  tryCatch(solve(t(X) %*% diag(as.vector(p * (1 - p))) %*% X + diag(1e-7, nrow = ncol(X))),
+                             error = function(e) "singular")
+        if (inverse == "singular") break
+      } else break
+    }
+
     beta <- beta + inverse %*% t(X) %*% (y - p)
     p <- 1 / (1 + exp(-X %*% beta))
   }
