2e54584ac276b652730e04cb500ab4f5fbd703d8
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-04-06 11:44:08 +0200

    added aggregation

--- _targets.R
+++ _targets.R
@@ -16,7 +16,8 @@ tar_option_set(
     "rlang",
     "purrr",
     "dplyr",
-    "readr"
+    "readr",
+    "tidyr"
   )
 )
 
@@ -34,14 +35,25 @@ list(
   #creditg
   tar_target(dataset_raw_file_creditg, "data/credit-g.csv", format = "file"),
   tar_target(dataset_raw_creditg, read_csv(dataset_raw_file_creditg)),
-  tar_target(X_creditg, dataset_raw_creditg[, -21] %>%
+  tar_target(X_creditg,
+             dataset_raw_creditg[, -21] %>%
                mutate(across(where(is.character), dummify)) %>%
                map_dfc(identity)),
   tar_target(y_creditg, dataset_raw_creditg[, 21] == "bad"),
 
-  tar_map(list(
-    algorithm = rlang::syms(rep(algorithm_names, each = length(dataset_names))),
-            X = rlang::syms(rep(paste0("X_", dataset_names), times = length(algorithm_names))),
-            y = rlang::syms(rep(paste0("y_", dataset_names), times = length(algorithm_names)))),
-    tar_target(CV, perform_CV(algorithm, X, y)))
-)
+  tar_map(
+    list(algorithm = rlang::syms(rep(algorithm_names, times = length(dataset_names))),
+         X = rlang::syms(rep(paste0("X_", dataset_names), each = length(algorithm_names))),
+         y = rlang::syms(rep(paste0("y_", dataset_names), each = length(algorithm_names))),
+         .datasetname = rlang::syms(rep(dataset_names, each = length(algorithm_names)))),
+    tar_target(CV, perform_CV(algorithm, X, y)),
+    names = c(algorithm, .datasetname)),
+
+  tar_map(
+    list(CV = rlang::syms(rlang::exec(paste0, !!!tidyr::expand_grid("CV_", algorithm_names, "_", dataset_names)))),
+    tar_target(
+      aggregated,
+      CV %>%
+        select(-fold) %>%
+        summarise(across(everything(), mean))))
+  )
