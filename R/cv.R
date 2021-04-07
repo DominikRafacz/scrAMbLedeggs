d82e471ff00f26671ccf6a0eb5b616233253f770
Laura <erdaradun.gaztea@gmail.com>
2021-04-07 13:55:30 +0200

    reworked _targets to make it clear

--- R/cv.R
+++ R/cv.R
@@ -1,11 +1,12 @@
 perform_CV <- function(algorithm, dataset, num_folds = 5, ...) {
-  algorithm_name <- enquo(algorithm)
+  algorithm_name <- algorithm[["name"]]
+  algorithm <- algorithm[["value"]][[1]]
   X <- dataset[["X"]]
   y <- dataset[["y"]]
   n <- nrow(X)
   inds <- sample(1:n)
   inds <- map(1:num_folds, function(ind) inds[seq(from = ind, to = n, by = num_folds)])
-
+  
   predictions <- map(inds, function(fold_ind) list(X = X[fold_ind, ], y = y[fold_ind])) %>%
     map(function(fold) algorithm(fold$X, fold$y, ...)) %>%
     map2(1:num_folds, function(model, ind) predict(model, X[-inds[[ind]],]))
@@ -20,10 +21,11 @@ perform_CV <- function(algorithm, dataset, num_folds = 5, ...) {
   
   null_log_likelihood <- map_dbl(1:num_folds, function(ind) log_likelihood(y[-inds[[ind]]], null_predictions[[ind]]$probability))
 
+  # TODO: add ... to tibble?
   tibble(fold = 1:num_folds) %>%
     mutate(data = attr(dataset, "name"),
            scaled = attr(dataset, "scaled"),
-           algorithm = as_label(algorithm_name),
+           algorithm = algorithm_name,
            accuracy = map_dbl(fold, map_measure(accuracy)),
            precision = map_dbl(fold, map_measure(precision)),
            recall = map_dbl(fold, map_measure(recall)),
